a36d7f1e260f25b4a94d91506afd8a1fd8e9cf20
yvexanagency <yvexanagency@gmail.com>
2026-07-02 20:16:13 +0100

    Implement opening range calculation

--- core/strategy.py
+++ core/strategy.py
@@ -1,3 +1,6 @@
+from datetime import time
+
+
 class ORBStrategy:
     def __init__(self, params):
         self.params = params
@@ -5,9 +8,23 @@ class ORBStrategy:
     def run(self, df):
         trades = []
 
-        grouped = df.groupby(df["timestamp ET"].dt.date)
+        or_minutes = self.params["or_minutes"]
 
-        for date, day in grouped:
-            print(f"{date} -> {len(day)} candles")
+        for date, day in df.groupby(df["timestamp ET"].dt.date):
+
+            # Keep only RTH
+            day = day.copy().reset_index(drop=True)
+
+            # Opening Range
+            opening_range = day.iloc[:or_minutes]
+
+            or_high = opening_range["high"].max()
+            or_low = opening_range["low"].min()
+
+            print(
+                f"{date} | "
+                f"OR High: {or_high:.2f} | "
+                f"OR Low: {or_low:.2f}"
+            )
 
         return trades
